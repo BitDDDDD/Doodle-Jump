abddda4a170daa79262a955c73262812128acfdf
BitDDDDD <162802022+BitDDDDD@users.noreply.github.com>
2024-06-27 11:32:02 +0300

    Update platformitems.cpp

--- platformitems.cpp
+++ platformitems.cpp
@@ -8,3 +8,6 @@ Rocket::Rocket(QGraphicsItem *parent)
 {
     setPixmap(QPixmap("://images/Rocket2.png").scaled((QSize(50,90))));
 }
+Bonker::Bonker(QgraphicsItem *parent)
+     setPixmap(QPixmap("://images/Bonker.png").scaled((QSize(50,100)));
+}
